658372e613818910bffa3f7ba5404d455f519f7b
Keaton Okkonen <keaton.okkonen@gmail.com>
2015-09-27 09:46:03 +1000

    changes

--- public/js/stream/stream.js
+++ public/js/stream/stream.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:80');
+var socket = io.connect('http://115.146.86.121:80');
 
 socket.on('webstream', function(data){
   console.log('Received result from server: ' + data)
